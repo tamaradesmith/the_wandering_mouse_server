8819af7b5edf8cae37f95aeede4bcf14ebb99316
Tamara Smith <tdesmith@shaw.ca>
2020-05-20 16:03:19 -0700

    debugging cors

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ app.use(express.json());
 app.use(logger('dev'));
 app.use(cors());
 
-app.options('*', cors())
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', "*");
-  next();
-});
-
-
 const score = require('./routes/score');
 app.use('/scores', score);
 
+app.use(function (res, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  console.log("err.status", err.status)
+  next(err);
+})
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
+})
 
 const PORT = 4040;
 const ADDRESS = 'https://wandering-mouse-server.herokuapp.com/';
