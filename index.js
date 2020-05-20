866ede9e018cba87f504a5f4585dce48d25a2e4b
Tamara Smith <tdesmith@shaw.ca>
2020-05-20 15:01:06 -0700

    debugging cors

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
-const PORT = 4040;
-const ADDRESS = 'https://wandering-mouse-server.herokuapp.com/';
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,12 +12,16 @@ app.use(cors());
 app.options('*', cors())
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', ADDRESS);
+  res.setHeader('Access-Control-Allow-Origin', "*");
   next();
 });
 
 const score = require('./routes/score');
 app.use('/scores', score);
+
+
+const PORT = 4040;
+const ADDRESS = 'https://wandering-mouse-server.herokuapp.com/';
 
 app.listen(PORT, ADDRESS, () => {
   console.log(`Listening => port: ${PORT} Address: ${ADDRESS}`)
